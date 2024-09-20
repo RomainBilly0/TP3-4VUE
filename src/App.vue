1390f4ca02efcfa26f37669e524fe71d139a528d
Romain Billy <romain.billy@efrei.net>
2024-09-20 15:22:54 +0200

    create base header and base footer

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-project empty
+  <div>
+    <base-header/>
+    <home-page />
+    <base-footer/>
+  </div>
 </template>
+<script setup lang="ts">
+import HomePage from "@/pages/HomePage.vue";
+</script>
